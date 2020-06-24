7711722fa66bd3547e4f8a7f712c07e3fb60b98b
github username <github email address>
2020-06-24 18:48:40 -0500

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,25 @@
+class StoreAdminController < ApplicationController
+    layout :resolve_layout
+    
+    def home
+    end
+
+    def orders
+    end
+    
+    def invoice
+    end
+
+    def resolve_layout
+        case action_name
+        when "home"
+            "admin"
+        when "orders"
+            "order_administration"
+        when "invoice"
+            false
+        else
+            "application"
+        end
+    end
+end
